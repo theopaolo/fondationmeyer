e4a95b191a3a6d632ba4564c02b7dd1d779aff20
mx <mx@suroh.tk>
2022-07-12 19:47:26 +0200

    added eslint file for js

--- _site/assets/js/client.js
+++ _site/assets/js/client.js
@@ -3,10 +3,10 @@ import Alpine from './alpine.js'
 document.addEventListener('alpine:init', () => {
   Alpine.store('year', {
     selected: undefined,
-    
+
     select(evt) {
       const el = evt.target
-      el.scrollIntoView({ block: 'end', inline: 'start', behavior: 'smooth' })
+      el.scrollIntoView({block: 'end', inline: 'start', behavior: 'smooth'})
     }
   })
 })
