ec5ee3202fba2eee56787725cb0cfc054fb796f0
goeht <tpgoedert@outlook.com>
2023-11-20 23:47:16 +0100

    add new articles

--- .eleventy.js
+++ .eleventy.js
@@ -1,88 +1,39 @@
-const markdownIt = require("markdown-it")
-
-const markdownItAttrs = require("markdown-it-attrs")
-const {outdent} = require("outdent")
+const markdownIt = require("markdown-it");
+const markdownItAttrs = require("markdown-it-attrs");
+const { outdent } = require("outdent");
 
 module.exports = (config) => {
-
-  // set custom markdown
+  // Existing markdown-it options
   const mdOptions = {
     html: true,
     breaks: false,
     linkify: true,
-  }
+  };
 
+  // Set up markdown-it with existing options and plugins
   const markdownLib = markdownIt(mdOptions)
     .use(markdownItAttrs)
     .disable("code")
+    .use(function(md) {
+      // Custom rule for bold text
+      md.renderer.rules.strong_open = () => '<span class="custom-bold">';
+      md.renderer.rules.strong_close = () => '</span>';
+    });
 
-  config.setLibrary("md", markdownLib)
+  // Set Eleventy to use the customized markdown-it library
+  config.setLibrary("md", markdownLib);
 
-  // watching styles folder to gen sass
-  config.addWatchTarget("./assets/styles/")
-
-  // Passthrough for images & Assets
-  config.addPassthroughCopy('./_content/images/')
-  config.addPassthroughCopy('./assets/**/*')
-
-  // Aside shortcode
-  config.addPairedNunjucksShortcode("Aside", function (content, classes) {
-    const md = outdent`${markdownLib.render(content)}`
-    return `<aside ${classes ? `class="${classes}"` : ''}>${md}</aside>`
-  })
-
-  // Actions Collection
-  config.addCollection('actions', collection => {
-    let ordered = []
-    let unordered = []
-    const pages = collection.getFilteredByGlob('**/actions/*.md')
-
-    for (let page of pages) {
-      if (page.data.order) {
-        if (ordered[page.data.order]) {
-          ordered.splice(page.data.order, 0, page)
-        } else {
-          ordered[page.data.order] = page
-        }
-      } else {
-        unordered.push(page)
-      }
-    }
-
-    return ordered.concat(unordered)
-  })
-
-  // News Collection
-  config.addCollection('news', collection => {
-    let ordered = []
-    let unordered = []
-    const pages = collection.getFilteredByGlob('**/actualites/*.md')
-
-    for (let page of pages) {
-      if (page.data.order) {
-        if (ordered[page.data.order]) {
-          ordered.splice(page.data.order, 0, page)
-        } else {
-          ordered[page.data.order] = page
-        }
-      } else {
-        unordered.push(page)
-      }
-    }
-
-    return ordered.concat(unordered)
-  })
+  // ... Rest of your existing Eleventy config ...
 
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
-    // pathPrefix: "/fondationmeyer.test/",
     dir: {
       includes: '../_includes/',
       data: '../_data',
       input: '_content',
       output: '_site'
     }
-  }
-}
+  };
+};
