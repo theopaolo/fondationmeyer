af9bb70d11c273561ea7a831d96f8d5fda964be6
goeht <tpgoedert@outlook.com>
2023-11-21 00:21:09 +0100

    update bold style

--- .eleventy.js
+++ .eleventy.js
@@ -16,14 +16,67 @@ module.exports = (config) => {
     .disable("code")
     .use(function(md) {
       // Custom rule for bold text
-      md.renderer.rules.strong_open = () => '<span class="custom-bold">';
+      md.renderer.rules.strong_open = () => '<span class="bold">';
       md.renderer.rules.strong_close = () => '</span>';
-    });
+  });
 
   // Set Eleventy to use the customized markdown-it library
-  config.setLibrary("md", markdownLib);
+  config.setLibrary("md", markdownLib)
 
-  // ... Rest of your existing Eleventy config ...
+  // watching styles folder to gen sass
+  config.addWatchTarget("./assets/styles/")
+
+  // Passthrough for images & Assets
+  config.addPassthroughCopy('./_content/images/')
+  config.addPassthroughCopy('./assets/**/*')
+
+  // Aside shortcode
+  config.addPairedNunjucksShortcode("Aside", function (content, classes) {
+    const md = outdent`${markdownLib.render(content)}`
+    return `<aside ${classes ? `class="${classes}"` : ''}>${md}</aside>`
+  })
+
+  // Actions Collection
+  config.addCollection('actions', collection => {
+    let ordered = []
+    let unordered = []
+    const pages = collection.getFilteredByGlob('**/actions/*.md')
+
+    for (let page of pages) {
+      if (page.data.order) {
+        if (ordered[page.data.order]) {
+          ordered.splice(page.data.order, 0, page)
+        } else {
+          ordered[page.data.order] = page
+        }
+      } else {
+        unordered.push(page)
+      }
+    }
+
+    return ordered.concat(unordered)
+  })
+
+  // News Collection
+  config.addCollection('news', collection => {
+    let ordered = []
+    let unordered = []
+    const pages = collection.getFilteredByGlob('**/actualites/*.md')
+
+    for (let page of pages) {
+      if (page.data.order) {
+        if (ordered[page.data.order]) {
+          ordered.splice(page.data.order, 0, page)
+        } else {
+          ordered[page.data.order] = page
+        }
+      } else {
+        unordered.push(page)
+      }
+    }
+
+    return ordered.concat(unordered)
+  })
 
   return {
     markdownTemplateEngine: 'njk',
